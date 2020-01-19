651ee0cbadb796cd1da94511996eebda7d79c035
Karol Lobos <karollobos@interia.pl>
2020-01-19 13:04:16 +0100

    Exchange funtion readFile on readFileSync

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ var server = http.createServer();
 server.on('request', function(request, response) {
     response.setHeader("Content-Type", "text/html; charset=utf-8");
     if (request.method === 'GET' && request.url === '/hello') {
-        response.write(fs.readFile('./index.html', 'utf-8', function(err, data) {
+        response.write(fs.readFileSync('./index.html', 'utf-8', function(err, data) {
             console.log(data);
         }));
     } else {
