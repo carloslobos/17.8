02e8220f9fc16640396e3d96ef20d945050a6042
Karol Lobos <karollobos@interia.pl>
2020-01-20 21:51:13 +0100

    EDIT

--- script.js
+++ script.js
@@ -8,13 +8,13 @@ var server = http.createServer();
 
 server.on('request', function(request, response) {
     response.setHeader("Content-Type", "text/html; charset=utf-8");
-    if (request.method === 'GET' && request.url === '/hello') {
-        response.write(fs.readFileSync('./index.html', 'utf-8', function(err, data) {
+    if (request.method === 'GET' && request.url === '/hello' && request.url === '/') {
+        response.write(fs.readFileSync('./index.html', function(err, data) {
             console.log(data);
         }));
     } else {
         response.statusCode = 404;
-        response.write('<h1>404: Bardzo Zła ścieżka!</h1>');
+        response.write('<h1>404: Bardzo Zła ścieżka!</h1><img src="sad-face.jpg">');
         response.end();
     }
 });
